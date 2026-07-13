525d97d9cf15d89d21a3320afa2abd8ef0b69b48
Dean Harris <rdeanharris@users.noreply.github.com>
2026-07-13 13:13:18 -0700

    Add Starfleet auth scaffold

---
+++ starfleet_auth_config.js
@@ -0,0 +1,14 @@
+window.GPU_TCO_STARFLEET_CONFIG = Object.freeze({
+  enabled: false,
+  providerName: "Starfleet",
+  authBaseUrl: "",
+  apiBaseUrl: "",
+  clientId: "",
+  redirectUri: "",
+  requiredEmailDomain: "nvidia.com",
+  inviteOnlyExternalUsers: true,
+  adminEmails: ["deanh@nvidia.com"],
+  adminGroup: "",
+  savedConfigurationsResource: "gpu-tco-configurations",
+  activityLogResource: "gpu-tco-activity",
+});
